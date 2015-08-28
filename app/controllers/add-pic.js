a6e5fa77c8ffb58c689d26bb315fba70e7d4fd96
Ryan Tanay <ryantanay@newevolution.org>
2015-08-28 17:34:20 -0500

    Fixed issues with script inclusion for the add page and its object

--- app/controllers/add-pic.js
+++ app/controllers/add-pic.js
@@ -4,20 +4,18 @@ app.controller("AddCtrl",
   "$firebaseArray",
   function($scope, $routeParams, $firebaseArray) {
 
-  var ref = new Firebase("picit-nss.firebaseio.com");
-    $scope.messages = $firebaseArray(ref);
-    $scope.addPic = function() {
-      $scope.messages.$add({
-        type: $scope.newItem.type,
-        type: $scope.newItem.url,
-        type: $scope.newItem.title,
-        type: $scope.newItem.boardId,
-        type: $scope.newItem.private,
-        type: $scope.newItem.userId,
-        type: $scope.newItem.description
-        
-      });
-    };
-  }
-
-  ]);
+  var ref = new Firebase("https://picit-nss.firebaseio.com/pics");
+  $scope.messages = $firebaseArray(ref);
+  $scope.newItem = {};
+  $scope.addPic = function() {
+    $scope.messages.$add({
+      type: $scope.newItem.type,
+      url: $scope.newItem.url,
+      title: $scope.newItem.title,
+      board: $scope.newItem.boardId,
+      private: $scope.newItem.private,
+      uid: $scope.newItem.userId,
+      description: $scope.newItem.description
+    });
+  };
+}]);
