981c3ff0af2bb4d83d150162ff34050a7cbb463e
Wayne Hutchinson <wfh2727@gmail.com>
2015-08-31 12:01:21 -0500

    need to change images from fixed size to scalable

--- app/controllers/initial-view.js
+++ app/controllers/initial-view.js
@@ -4,12 +4,12 @@ app.controller("InitCtrl",
     $scope.boards = $firebaseArray(ref);
     console.log($scope.boards);
 
-  $(document).on('mouseenter', "div.pin", function() {
-    $(".tack").show();
+  $(".initial-view").on('mouseenter', "div.pin", function() {
+    $(this).children(".tack").removeClass("invisible");
   });  
 
-  $(document).on('mouseleave', ".pin", function() {
-    $(".tack").hide();
+  $(".initial-view").on('mouseleave', ".pin", function() {
+    $(this).children(".tack").addClass("invisible");
   });
   
   // $scope.onMouseEnter = function showTack() {
