ec1f7833f8165a25fad4e593aa166c8ed7e925ac
Lynn Samuelson <lynn.samuelson@gmail.com>
2015-08-31 09:15:48 -0500

    fixed form

--- app/controllers/add-pic.js
+++ app/controllers/add-pic.js
@@ -4,18 +4,20 @@ app.controller("AddCtrl",
   "$firebaseArray",
   function($scope, $routeParams, $firebaseArray) {
 
-  var ref = new Firebase("picit-nss.firebaseio.com");
+  var ref = new Firebase("https://picit-nss.firebaseio.com/pics");
     $scope.messages = $firebaseArray(ref);
+    $scope.newItem = {};
     $scope.addPic = function() {
       $scope.messages.$add({
+
         type: $scope.newItem.type,
-        type: $scope.newItem.url,
-        type: $scope.newItem.title,
-        type: $scope.newItem.boardId,
-        type: $scope.newItem.private,
-        type: $scope.newItem.userId,
-        type: $scope.newItem.description
-        
+        url: $scope.newItem.url,
+        title: $scope.newItem.title,
+        boardId: $scope.newItem.boardId,
+        private: $scope.newItem.private,
+        userId: $scope.newItem.userId,
+        description: $scope.newItem.description
+
       });
     };
   }
